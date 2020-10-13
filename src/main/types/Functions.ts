ca34163480553b42adf2e972e392065803a6f6e9
UnDer-7 <mateus7532@gmail.com>
2020-10-12 21:40:17 -0300

    fix

--- src/main/types/Functions.ts
+++ src/main/types/Functions.ts
@@ -1,8 +1,10 @@
 export type Consumer<Param> = (t: Param) => void;
 export type BiConsumer<ParamA, ParamB> = (t: ParamA, u: ParamB) => void;
+export type OptionalConsumer<Param> = (t?: Param) => void;
+export type OptionalBiConsumer<ParamA, ParamB> = (t?: ParamA, u?: ParamB) => void;
 
 export type Func<Param, Return> = (t: Param) => Return;
-export type OptinalFunc<Param, Return> = (t?: Param) => Return;
+export type OptionalFunc<Param, Return> = (t?: Param) => Return;
 export type BiFunc<ParamA, ParamB, Return> = (t: ParamA, u: ParamB) => Return;
 export type TriFunc<ParamA, ParamB, ParamC, Return> = (a: ParamA, b: ParamB, c: ParamC) => Return;
 
