69fb5fba6ee4d3742e345c7fa7a69e44908c84f9
UnDer-7 <mateus7532@gmail.com>
2020-10-14 19:46:12 -0300

    adding Optional nos functions

--- src/main/types/Functions.ts
+++ src/main/types/Functions.ts
@@ -1,17 +1,24 @@
 export type Consumer<Param> = (t: Param) => void;
 export type BiConsumer<ParamA, ParamB> = (t: ParamA, u: ParamB) => void;
 export type OptionalConsumer<Param> = (t?: Param) => void;
-export type OptionalBiConsumer<ParamA, ParamB> = (t?: ParamA, u?: ParamB) => void;
+export type OptionalBiConsumer<ParamA, ParamB> = (t: ParamA, u?: ParamB) => void;
+export type OptionalAllBiConsumer<ParamA, ParamB> = (t?: ParamA, u?: ParamB) => void;
 
 export type Func<Param, Return> = (t: Param) => Return;
-export type OptionalFunc<Param, Return> = (t?: Param) => Return;
 export type BiFunc<ParamA, ParamB, Return> = (t: ParamA, u: ParamB) => Return;
 export type TriFunc<ParamA, ParamB, ParamC, Return> = (a: ParamA, b: ParamB, c: ParamC) => Return;
+export type OptionalFunc<Param, Return> = (t?: Param) => Return;
+export type OptionalBiFunc<ParamA, ParamB, Return> = (t: ParamA, u?: ParamB) => Return;
+export type OptionalAllBiFunc<ParamA, ParamB, Return> = (t?: ParamA, u?: ParamB) => Return;
 
 export type UnaryOperator<ParamReturn> = Func<ParamReturn, ParamReturn>;
+export type OptionalUnaryOperator<ParamReturn> = OptionalFunc<ParamReturn, ParamReturn>;
 
 export type Predicate<Param> = (t: Param) => boolean;
 export type BiPredicate<ParamA, ParamB> = (t: ParamA, u: ParamB) => boolean;
+export type OptionalPredicate<Param> = (t: Param) => boolean;
+export type OptionalBiPredicate<ParamA, ParamB> = (t: ParamA, u?: ParamB) => boolean;
+export type OptionalAllBiPredicate<ParamA, ParamB> = (t?: ParamA, u?: ParamB) => boolean;
 
 export type Supplier<Return> = () => Return;
 export type Runnable = () => void;
